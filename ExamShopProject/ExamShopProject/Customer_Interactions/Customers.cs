f9a2762b985996bcc7a2159dba8a4dffadae7203
Helena Brunsgaard Madsen <hele1564@student.eadania.dk>
2018-05-07 15:28:14 +0200

    Minor changes in Customer - Customer_interface

--- ExamShopProject/ExamShopProject/Customer_Interactions/Customers.cs
+++ ExamShopProject/ExamShopProject/Customer_Interactions/Customers.cs
@@ -13,20 +13,21 @@ namespace ExamShopProject.Customer_Interactions
         Customer customer = new Customer();
         string name, adress, contactInfo, spokesPerson;
         float annualIncome;
+        int customerID;
 
-        private void ViewCustomer()
-        {
-            // DB command
-        }
-        private void CreateCustomer()
+        private void CreateCustomer(string name, string adress, string contactInfo, string spokesPerson)
         {
             // DB command (name, adress, contactInfo, spokesPerson)
         }
-        private void EditCustomer()
+        private void EditCustomer(int IDToEdit)
         {
             // DB command(IDToEdit)
         }
-        private void DeleteCustomer()
+        private void DeleteCustomer(int IDToDelete)
+        {
+            // DB command(IDToDelete)
+        }
+        private void ViewCustomer()
         {
             // DB command
         }
