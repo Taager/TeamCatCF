a1dd3f763bc7b59c1673d4f5312cfb13dc175d59
Mikkel Glerup <Mikkel Glerup@MIKKELGLERUP>
2018-05-07 13:44:50 +0200

    Added log and DB

---
+++ ExamShopProject/ExamShopProject/DB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamShopProject
+{
+    // made by Mikkel. E.R. Glerup
+    static class DB
+    {
+    }
+}
---
+++ ExamShopProject/ExamShopProject/ErrorHandler/Log.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamShopProject.ErrorHandler
+{
+    static class Log
+    {
+        // made by Mikkel. E.R. Glerup
+        public static void WriteFail()
+        {
+            DateTime currentTime = DateTime.Now;
+            string path = @"C:\Users\Fluffy\source\repos\AP-EngrossBETA\MyTest";
+            if (!File.Exists(path))
+            {
+
+                string createText = "Test" + Environment.NewLine;
+                File.WriteAllText(path, createText);
+            }
+            string appendText = "Test Exeption" + currentTime.ToString() + Environment.NewLine;
+            File.AppendAllText(path, appendText);
+        }
+    }
+}
