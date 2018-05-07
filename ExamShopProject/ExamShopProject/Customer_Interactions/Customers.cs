e003d741be366748e1612c3f0f610a13903bdf8c
Helena Brunsgaard Madsen <hele1564@student.eadania.dk>
2018-05-07 14:37:07 +0200

    Created Customer under Customer_Interactions

---
+++ ExamShopProject/ExamShopProject/Customer_Interactions/Customers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExamShopProject.Object;
+
+namespace ExamShopProject.Customer_Interactions
+{
+    // Made by Helena Brunsgaard Madsen
+    class Customers
+    {
+        Customer customer = new Customer();
+        string name, adress, contactInfo, spokesPerson;
+        float annualIncome;
+
+        private void ViewCustomer()
+        {
+            // DB command
+        }
+        private void CreateCustomer()
+        {
+            // DB command (name, adress, contactInfo, spokesPerson)
+        }
+        private void EditCustomer()
+        {
+            // DB command(IDToEdit)
+        }
+        private void DeleteCustomer()
+        {
+            // DB command
+        }
+        private void ViewCustomerStat()
+        {
+            // Stat command
+        }
+        public void Log()
+        {
+            // Log command
+        }
+
+    }
+}
