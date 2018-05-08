524dabee53d54ea60d41f6f72d81f9ecb1ae59a8
Helena Brunsgaard Madsen <hele1564@student.eadania.dk>
2018-05-08 13:56:30 +0200

    Minor changes in CreateCustomer

--- ExamShopProject/ExamShopProject/Visual_Customer/CreateCustomer.xaml.cs
+++ ExamShopProject/ExamShopProject/Visual_Customer/CreateCustomer.xaml.cs
@@ -13,19 +13,26 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using ExamShopProject.Object;
+using ExamShopProject.Customer_Interactions;
 
 namespace ExamShopProject
 {
-    /// <summary>
-    /// Interaction logic for CreateCustomer.xaml
-    /// </summary>
+    // Made By Helena Brunsgaard Madsen
     public partial class CreateCustomer : Page
     {
         Customer customer = new Customer();
+        CustomerLogic interaction = new CustomerLogic();
         public CreateCustomer()
         {
             InitializeComponent();
             DataContext = customer;
+        }
+
+        private void btn_Save_Click(object sender, RoutedEventArgs e)
+        {
+            interaction.CreateCustomer(customer.Name, customer.Adress, customer.ContactInfo, customer.SpokesPerson, customer.AnnualIncome);
+            MessageBox.Show("Successfully saved!");
+            this.Content = null
         }
     }
 }
