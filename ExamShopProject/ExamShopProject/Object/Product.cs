e41b41d09df15f3045d61cca8fb32e6bd4dfca68
Helena Brunsgaard Madsen <hele1564@student.eadania.dk>
2018-05-16 14:29:42 +0200

    Products

--- ExamShopProject/ExamShopProject/Object/Product.cs
+++ ExamShopProject/ExamShopProject/Object/Product.cs
@@ -1,18 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ExamShopProject.Object
 {
-    //lavet af Mikkel E.R. Glerup
+    //Made by Mikkel E.R. Glerup
     class Product
     {
-        public int price { get; set; }
-        public int ID { get; set; }
-        public string name { get; set; }
-        public int category { get; set; }
-        public string description { get; set; }
+        private int _price;
+        public int Price
+        {
+            get { return _price; }
+            set
+            {
+                _price = value;
+                NotifyPropertyChanged();
+            }
+        }
+        private int _iD;
+        public int ID
+        {
+            get { return _iD; }
+            set
+            {
+                _iD = value;
+                NotifyPropertyChanged();
+            }
+        }
+        private int _name;
+        public int Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                NotifyPropertyChanged();
+            }
+        }
+        private int _category;
+        public int Category
+        {
+            get { return _category; }
+            set
+            {
+                _category = value;
+                NotifyPropertyChanged();
+            }
+        }
+        private int _description;
+        public int Description
+        {
+            get { return _description; }
+            set
+            {
+                _description = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
     }
 }
---
+++ ExamShopProject/ExamShopProject/Product_Interactions/ProductLogic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamShopProject.Product_Interactions
+{
+    // Made by Helena Brunsgaard Madsen
+    class ProductLogic
+    {
+        private void CreateProduct()
+        {
+
+        }
+        private void EditProduct()
+        {
+
+        }
+        private void DeleteProduct()
+        {
+
+        }
+        private void UpdateProduct()
+        {
+
+        }
+        private void CalcTax()
+        {
+
+        }
+        private void CheckDiscount()
+        {
+
+        }
+    }
+}
