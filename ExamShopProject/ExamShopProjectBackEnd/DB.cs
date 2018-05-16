4bf53eea93fb8d6d7e6382f2caea71622e6f8c05
Taager <taager2201@gmail.com>
2018-05-16 09:40:24 +0200

    Working on backend

---
+++ ExamShopProject/ExamShopProjectBackEnd/DB.cs
@@ -0,0 +1,54 @@
+﻿using System;
+//using ExamShopProject.ErrorHandler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.Common;
+
+namespace ExamShopProjectBackEnd
+{
+    // made by Brian K. Petersen
+    static class DB
+    {
+        #region open and close connection
+        //made by Mikkel. E.R. Glerup
+        public static SqlConnection myConnection;
+        private static bool openConnection()
+        {
+            try
+            {
+                myConnection = new SqlConnection(
+                    "Data Source=.;Initial Catalog=Charlie-APE;Integrated Security=True"
+                    );
+                myConnection.Open();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //Log.WriteFail(ex);
+                return false;
+            }
+        }
+        private static bool closeConnection()
+        {
+            try
+            {
+                myConnection.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //Log.WriteFail(ex);
+                return false;
+            }
+        }
+        #endregion
+
+        #region import/export catalogue
+
+        #endregion
+    }
+}
---
+++ ExamShopProject/ExamShopProjectBackEnd/FileManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Renci.SshNet;
+
+namespace ExamShopProjectBackEnd
+{
+    class FileManager
+    {
+        private static SftpClient ConnectMe()
+        {
+            SftpClient client = new SftpClient("10.152.120.37", "charlie", "char1234lie");
+            return client;
+        }
+        public static bool FetchFile()
+        {
+            SftpClient client = ConnectMe();
+            client.Connect();
+            string remoteDirectory = "/home/indbakke/";
+            string finalDir = "";
+            string localDirectory = @"";
+            using (var sftp = client)
+            {
+                //Console.WriteLine("Connecting to " + host + " as " + username);
+                //sftp.Connect();
+                //Console.WriteLine("Connected!");
+                var files = sftp.ListDirectory(remoteDirectory);
+
+                foreach (var file in files)
+                {
+
+                    string remoteFileName = file.Name;
+
+                    if ((!file.Name.StartsWith(".")) /*&& ((file.LastWriteTime.Date == DateTime.Today))*/) //The date check needs to be reimplemented
+                    {
+
+                        finalDir = localDirectory;
+
+
+                        if (File.Exists(finalDir + file.Name))
+                        {
+                            //Console.WriteLine("File " + file.Name + " Exists");
+                        }
+                        else
+                        {
+                            //Console.WriteLine("Downloading file: " + file.Name);
+                            using (Stream file1 = File.OpenWrite(finalDir + remoteFileName))
+                            {
+                                sftp.DownloadFile(remoteDirectory + remoteFileName, file1);
+                            }
+                        }
+                    }
+                }
+            }
+            
+            return true;
+        }
+    }
+}
